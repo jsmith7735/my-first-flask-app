2261368f86ea21b4bdeae787d82ded64fac8edbe
jsmith7735 <jsmith7735@gmail.com>
2024-08-28 21:46:49 +0000

    DigialOcean: How To Handle Errors in a Flash Application

--- main.py
+++ main.py
@@ -1,48 +1,37 @@
-import os
+from flask import Flask, render_template, abort
 
-from flask import Flask, send_file, abort, render_template
-from markupsafe import escape
-import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index/")
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+@app.route('/')
 def index():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html')
 
-@app.route("/about/")
-def about():
-    return render_template('about.html')
+# ...
 
-@app.route("/capitalize/<word>/")
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+# ...
 
-@app.route("/add/<int:n1>/<int:n2>/")
-def add(n1, n2):
-    return '<h1>{}</h1>'.format(n1 + n2)
-
-@app.route("/users/<int:user_id>/")
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
+@app.route('/messages/<int:idx>')
+def message(idx):
+    app.logger.info('Building the messages list...')
+    messages = ['Message Zero', 'Message One', 'Message Two']
     try:
-        return '<h1>Hi {}</h1>'.format(users[user_id])
+        app.logger.debug('Get message with index: {}'.format(idx))
+        return render_template('message.html', message=messages[idx])
     except IndexError:
+        app.logger.error('Index {} is causing an IndexError'.format(idx))
         abort(404)
 
-@app.route('/comments/')
-def comments():
-    comments = ['This is the first comment.',
-                'This is the second comment.',
-                'This is the third comment.',
-                'This is the fourth comment.'
-                ]
-
-    return render_template('comments.html', comments=comments)
-
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
-
-if __name__ == "__main__":
-    main()
+# ...
+# ...
+@app.route('/500')
+def error500():
+    abort(500)
