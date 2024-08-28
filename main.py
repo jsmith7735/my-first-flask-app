534874953f4258160229a2f49ef43f471843ad64
jsmith7735 <jsmith7735@gmail.com>
2024-08-28 02:52:42 +0000

    DigitalOcean: How to Use Templates in a Flask Application

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 
-from flask import Flask, send_file, abort
+from flask import Flask, send_file, abort, render_template
 from markupsafe import escape
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 @app.route("/about/")
 def about():
-    return '<h3>This is a Flask Web Application</h3>'
+    return render_template('about.html')
 
 @app.route("/capitalize/<word>/")
 def capitalize(word):
@@ -29,6 +30,16 @@ def greet_user(user_id):
         return '<h1>Hi {}</h1>'.format(users[user_id])
     except IndexError:
         abort(404)
+
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+
+    return render_template('comments.html', comments=comments)
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
